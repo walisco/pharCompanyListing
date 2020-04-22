6f025c997c4212af555f64fd2dac49d9e9de421c
walisco <wale719@gmail.com>
2020-04-22 16:46:49 +0100

    updated test

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,4 +1,5 @@
 const assert = require("assert");
+const fs = require('fs');
 
 describe("check page title", () => {
     it("should have the correct title", () => {
@@ -13,34 +14,45 @@ describe("Browse pharmaceutical company names", () => {
     browser.url("https://www.medicines.org.uk/emc/browse-companies/");
     const companiesBeginingWith = $$("ul.browse li a");
     const listingName = $$("a.key");
-    const contactInfo = $$('.gfdCompanyDetailsCol')
-    const companyDetail = $$('p')
-    const tableHeader = $$('.gfdCompanyDetailsCol div')
+    const contactInfo = $$('.gfdCompanyDetailsCol');
+    const companyDetail = $$('p');
+    const comDetails = $$('.gfdCompanyDetailsTitle +div p')
+    const tableHeader = $$('.gfdCompanyDetailsCol div');
+    const companyHeader = $$('.gfdCompanyDetailsTitle');
     let jsonStr = {}
     let obj = []
 
-    for (i = 0; i < 2; i++) {
+    for (i = 0; i < companiesBeginingWith.length; i++) {
       companiesBeginingWith[i].click();
       for (j = 0; j < listingName.length; j++) {
         if (listingName[j].index == 0) {
           listingName[j].click();
-          companyDetail.forEach(add => jsonStr = "{["+"address:"+'"'+add.getText()+'"'+"}]");
-          let temp =JSON.parse(JSON.stringify(jsonStr))
-          obj.push(temp)
-          console.log("**", obj)
-          
-          
-          // tableHeader.forEach(add => add.getText())
-        
+         
 
-        //   companyDetail.forEach(add => console.log("***",add.getText()))
+
+          for(k=0; k < $$('.gfdCompanyDetailsTitle').length; k++) {
+            let key = $$('.gfdCompanyDetailsTitle')[k].getText()
+            let value = $$('p')[k].getText();
+            jsonStr = "{"+'"'+key+'":'+'"'+value+'"'+"}";
+            let temp =JSON.parse(JSON.stringify(jsonStr))
+            obj.push(temp)
+            console.log("**", obj)
+
+            fs.writeFile ("company.json", obj, 'utf8', function (err) {
+              if (err) {
+                  console.log("An error occured while writing JSON Object to File.");
+                  return console.log(err);
+              }
+           
+              console.log("JSON file has been saved.");
+          });
+          }
+         
           browser.back();
         }
       
         if (listingName[j].index == 2) {
           listingName[j].click();
-          // companyDetail.forEach(add => obj = JSON.parse("{["+"address:"+add.getText()+"}]"));
-        //   contactInfo.forEach(contact => console.log("???",contact.getAttribute('innerText')))
           browser.back();
         }
        /*  if (listingName[j].index == listingName.length) {
